c6369d61cf09504bdad67ea2a2b5895572b189aa
VaporSiriz <vaporsiriz@gmail.com>
2020-11-17 21:43:52 +0900

    빼먹은 vote 파일

---
+++ Election/views/vote.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, make_response
+from models import *
+
+vote_page = Blueprint('vote_page', __name__, template_folder='templates', static_folder='static')
+
+@vote_page.route('/')
+def index():
+    return render_template('views/vote/index.html')
+
+
+
+@vote_page.route('status')
+def status():
+    return 'OK', 200
