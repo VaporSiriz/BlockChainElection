be461f3a2cadb1f28a9a9e7fd32e745037b6487a
Gyuparupa <tuvisa96@gmail.com>
2020-11-22 20:06:55 +0900

    CSRF추가, election blueprint추가

--- Election/election.py
+++ Election/election.py
@@ -1,10 +1,11 @@
 from datetime import datetime
  
 from flask import Flask, render_template
-from views import login, vote, index, message
+from views import login, vote, index, election, message
 from models import db, db_commit, db_end
 from login_manager import login_manager
 from flask_util_js import FlaskUtilJs
+from flask_wtf.csrf import CSRFProtect
 import debugpy
 
 app = Flask(__name__)
@@ -13,7 +14,10 @@ app.register_blueprint(index.index_page, url_prefix='/')
 app.register_blueprint(login.login_page, url_prefix='/login')
 app.register_blueprint(vote.vote_page, url_prefix='/vote')
 app.register_blueprint(message.message_page, url_prefix='/message')
+app.register_blueprint(election.election_page, url_prefix='/election')
 
+csrf = CSRFProtect()    # 사이트위조공격 방지
+csrf.init_app(app)
 #debugpy.listen(("0.0.0.0", 5678))
 
 def init_app():
