5b9c99bd4873903cc8379401bad30ece3bb7c6bc
Gyuparupa <tuvisa96@gmail.com>
2020-11-22 20:11:29 +0900

    선거 생성페이지에 필요한 form(AddElectionForm) 추가

--- Election/views/forms.py
+++ Election/views/forms.py
@@ -1,6 +1,15 @@
-from wtforms import Form, BooleanField, TextField, StringField, PasswordField, SelectField, TextAreaField, validators
+from wtforms import Form, BooleanField, TextField, StringField, PasswordField, SelectField, TextAreaField, validators, DateTimeField, DateField
+from wtforms.fields.html5 import DateTimeLocalField
+
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
 
 class UserLoginForm(Form):
     username = StringField('user_id', validators=[validators.InputRequired('')])
     password = PasswordField('password', validators=[validators.InputRequired('')])
 
+class AddElectionForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    desc = StringField('desc', validators=[DataRequired()])
+    startat = DateTimeLocalField('startat', validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
+    endat = DateTimeLocalField('endat', validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
