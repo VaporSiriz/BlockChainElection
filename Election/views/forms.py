f3adc5ecdebabc325a10e8a8db5d93dbfd3728dd
Gyuparupa <tuvisa96@gmail.com>
2020-11-24 16:51:11 +0900

    선거 수정페이지 폼(modify) 추가

--- Election/views/forms.py
+++ Election/views/forms.py
@@ -6,7 +6,13 @@ class UserLoginForm(Form):
     password = PasswordField('password', validators=[validators.InputRequired('')])
 
 class AddElectionForm(Form):
-    title = StringField('title', validators=[validators.InputRequired('선거 이름을 입력해주세요')])
-    desc = StringField('desc', validators=[validators.InputRequired('선거 내용을 입력해주세요')])
-    startat = DateTimeLocalField('startat', validators=[validators.InputRequired('시작 시간을 입력해주세요')], format='%Y-%m-%dT%H:%M')
-    endat = DateTimeLocalField('endat', validators=[validators.InputRequired('종료 시간을 입력해주세요')], format='%Y-%m-%dT%H:%M')
+    title = StringField('title', validators=[validators.InputRequired()])
+    desc = StringField('desc', validators=[validators.InputRequired()])
+    startat = DateTimeLocalField('startat', validators=[validators.InputRequired()], format='%Y-%m-%dT%H:%M')
+    endat = DateTimeLocalField('endat', validators=[validators.InputRequired()], format='%Y-%m-%dT%H:%M')
+
+class ModifyElectionForm(Form):
+    title = StringField('title', validators=[validators.InputRequired()])
+    desc = StringField('desc', validators=[validators.InputRequired()])
+    startat = DateTimeLocalField('startat', validators=[validators.InputRequired()], format='%Y-%m-%dT%H:%M')
+    endat = DateTimeLocalField('endat', validators=[validators.InputRequired()], format='%Y-%m-%dT%H:%M')
