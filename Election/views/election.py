3f36ce96c41a3ed1e060bf2c1d3388dcb855fa8e
Gyuparupa <tuvisa96@gmail.com>
2020-11-24 16:52:30 +0900

    선거 수정 페이지 및 기능 추가

--- Election/views/election.py
+++ Election/views/election.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, make_response, redirect, request
+from flask.helpers import url_for
 from models import *
-from .forms import AddElectionForm
+from .forms import AddElectionForm, ModifyElectionForm
 from datetime import datetime, timedelta
 
 election_page = Blueprint('election_page', __name__, template_folder='templates', static_folder='static')
@@ -54,3 +55,18 @@ def manageElection():
     end_list = end_list.paginate(page2, per_page=4)
 
     return render_template('views/election/manage.html', res_list=res_list, end_list=end_list)
+
+@election_page.route('/modify/<int:id>', methods=['GET', 'POST'])
+def modifyElection(id):
+    form = ModifyElectionForm(request.form)
+    data = Election.query.get(id)
+    if form.validate():
+        election = Election.query.get(id)
+        election.title = form['title'].data
+        election.desc = form['desc'].data
+        election.startat = form['startat'].data
+        election.endat = form['endat'].data
+        db.session.commit()
+
+        return redirect(url_for('election_page.manageElection'))
+    return render_template('views/election/modify.html', form=form, data=data)
