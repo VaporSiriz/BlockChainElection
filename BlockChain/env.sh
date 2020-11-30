423ee68784da041e8a85156fd80683c9690ba0d8
VaporSiriz <vaporsiriz@gmail.com>
2020-12-01 07:22:07 +0900

    number도 추가

--- BlockChain/env.sh
+++ BlockChain/env.sh
@@ -1,3 +1,4 @@
+export server_number=$1
 export BlockChainURL1=ec2-3-94-55-102.compute-1.amazonaws.com
 export BlockChainURL2=ec2-54-157-175-80.compute-1.amazonaws.com
-
+export BlockChainURL3=ec2-54-227-197-86.compute-1.amazonaws.com
